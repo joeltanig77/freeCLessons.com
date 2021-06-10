cbf05287e554fba9ff1b89aef3be61132747d241
Joel Tanig <joeltanig77@gmail.com>
2021-06-10 08:46:56 -0700

    implemented some ace for testing

--- aceEditor/js/aceEditor.js
+++ aceEditor/js/aceEditor.js
@@ -1,2 +1,3 @@
 let codeEditor = ace.edit("mainEditWindow");
-codeEditor.session.setMode("../ace-editor/src-min/mode/c_cpp.js")
+codeEditor.session.setMode("../ace-editor/src-min/mode/c_cpp")
+editor.setTheme("ace/theme/monokai");
